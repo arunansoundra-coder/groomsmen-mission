70b8e0f0bd3c31934e498cd730383b8fae2b6644
arunansoundra-coder <arunansoundra@gmail.com>
2026-03-23 10:23:25 -0500

    Update sounds.js

--- sounds.js
+++ sounds.js
@@ -1,2 +1,8 @@
 export const dealSound = new Audio("https://assets.mixkit.co/active_storage/sfx/2005/2005-preview.mp3");
 export const chipSound = new Audio("https://assets.mixkit.co/active_storage/sfx/209/209-preview.mp3");
+export function playAmbient() {
+  const audio = new Audio('ambient.mp3');
+  audio.loop = true;
+  audio.volume = 0.2;
+  audio.play();
+}
