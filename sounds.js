686e3e4f37d0b7b0395bde0fe22cd12f4e60da76
arunansoundra-coder <arunansoundra@gmail.com>
2026-03-22 20:56:23 -0500

    Create sounds.js

---
+++ sounds.js
@@ -0,0 +1,2 @@
+export const dealSound = new Audio("https://assets.mixkit.co/active_storage/sfx/2005/2005-preview.mp3");
+export const chipSound = new Audio("https://assets.mixkit.co/active_storage/sfx/209/209-preview.mp3");
