35b102271a5273f304e61560af61201dc48c8316
arunansoundra-coder <arunansoundra@gmail.com>
2026-03-22 20:56:06 -0500

    Update and rename script.js to main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+import { startQuestions } from './questionsUI.js';
+import { startPoker } from './pokerUI.js';
+
+const app = document.getElementById('app');
+
+function loadScreen(screen) {
+  app.innerHTML = '';
+
+  if (screen === 'questions') startQuestions(app, () => {
+    loadScreen('poker');
+  });
+
+  if (screen === 'poker') startPoker(app);
+}
+
+// Decide starting screen
+loadScreen('questions');
