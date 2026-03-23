b7e78ff1de99e1b7aca4974ec4ee8a9fc85f5c0a
arunansoundra-coder <arunansoundra@gmail.com>
2026-03-22 20:58:14 -0500

    Create questionsUI.js

---
+++ questionsUI.js
@@ -0,0 +1,43 @@
+import { questions } from './questions.js';
+
+let currentQ = 0;
+
+export function startQuestions(app, onComplete){
+
+  function render(){
+    const q = questions[currentQ];
+
+    app.innerHTML = `
+      <h2>MISSION LEVEL ${currentQ+1}</h2>
+      <p>${q.q}</p>
+
+      ${q.options.map(o=>`
+        <button onclick="checkAnswer('${o}')">${o}</button>
+      `).join('')}
+
+      <div id="feedback"></div>
+    `;
+  }
+
+  window.checkAnswer = function(ans){
+    const q = questions[currentQ];
+    const fb = document.getElementById("feedback");
+
+    if(ans === q.answer){
+      currentQ++;
+
+      if(currentQ === questions.length){
+        fb.innerText = "✓ Access Granted";
+        setTimeout(onComplete, 800);
+      } else {
+        fb.innerText = "✓ Correct";
+        setTimeout(render, 500);
+      }
+
+    } else {
+      fb.innerText = "✖ Try Again";
+    }
+  };
+
+  render();
+}
