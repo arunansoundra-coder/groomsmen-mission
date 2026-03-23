a76e284b313caccc1f34c191a5585a68ead0833a
arunansoundra-coder <arunansoundra@gmail.com>
2026-03-22 20:57:56 -0500

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,8 @@
+export const questions = [
+  {
+    q: "The operation begins in which city?",
+    options: ["Cedar Rapids","Chicago","Des Moines","Omaha"],
+    answer: "Cedar Rapids"
+  },
+  // keep the rest here...
+];
