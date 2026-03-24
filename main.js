a73f8fedb29fa0b730453374c8e188eb5345acf3
arunansoundra-coder <arunansoundra@gmail.com>
2026-03-23 21:31:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,22 +6,3 @@ const app = document.getElementById("app");
 
 const params = new URLSearchParams(window.location.search);
 const agentName = params.get("agent") || "Agent";
-
-let index = 0;
-const agents = ["Jason","Josh","Duran","Taylor","Gill","Prathap"];
-
-function nextAgent(){
-  if (index < agents.length){
-    app.innerHTML = "";
-
-    startBriefing(app, agents[index], () => {
-      index++;
-      nextAgent();
-    });
-
-  } else {
-    startPoker(app);
-  }
-}
-
-nextAgent();
