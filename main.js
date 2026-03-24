d76fecf057010c5da0193c8bc6905bd18c0e3ac0
arunansoundra-coder <arunansoundra@gmail.com>
2026-03-23 20:55:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-import { loadScreen } from "./screenManager.js";
+import { startQuestions } from "./questionsUI.js";
 import { startBriefing } from "./briefingUI.js";
 import { startPoker } from "./pokerUI.js";
 
 const app = document.getElementById("app");
 
-const params = new URLSearchParams(window.location.search);
-const agentName = params.get("agent") || "Agent";
-
-const agents = ["Jason","Josh","Duran","Taylor","Gill","Prathap"];
-
+let agents = ["Jason","Josh","Duran","Taylor","Gill","Prathap"];
 let index = 0;
+
+function startFlow() {
+  startQuestions(app, () => {
+    nextAgent();
+  });
+}
 
 function nextAgent(){
   if (index < agents.length){
     app.innerHTML = "";
+
     startBriefing(app, agents[index], () => {
       index++;
       nextAgent();
     });
+
   } else {
     startPoker(app);
   }
 }
 
-nextAgent();
+startFlow();
