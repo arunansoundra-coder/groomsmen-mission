6af268886abbfd9b7a85adcc58602cd393d61d5a
arunansoundra-coder <arunansoundra@gmail.com>
2026-03-28 16:39:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,16 +8,12 @@ const app = document.getElementById("app");
 const params = new URLSearchParams(window.location.search);
 const agentName = params.get("agent") || "Agent";
 
-// Step 1 → Questions
 startQuestions(app, () => {
 
-  // Step 2 → Briefing (for THIS agent only)
   startBriefing(app, agentName, () => {
 
-    // Step 3 → Mission Objective (NEW SCREEN)
     startMissionObjective(app, agentName, () => {
 
-      // Step 4 → Poker
       startPoker(app);
 
     });
