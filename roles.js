36afe37435e43bd69ae2179f7e61ee1b3f58edcd
arunansoundra-coder <arunansoundra@gmail.com>
2026-04-04 17:57:36 -0500

    Update and rename agents.js to roles.js

---
+++ roles.js
@@ -0,0 +1,9 @@
+export const roles = {
+  Jason: "Best Man",
+  Arunan: "Groom",
+  Gill: "Groomsman",
+  Prathap: "Groomsman",
+  Taylor: "Groomsman",
+  Duran: "Groomsman",
+  Josh: "Groomsman"
+};
