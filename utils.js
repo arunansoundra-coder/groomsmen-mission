de35b4037849e8b1727294c9620c5e05c1c51f02
arunansoundra-coder <arunansoundra@gmail.com>
2026-04-04 18:28:19 -0500

    Delete utils.js

--- utils.js
+++
@@ -1,7 +0,0 @@
-export function normalizeText(str = "") {
-  return str.toLowerCase().trim().replace(/\s+/g, " ");
-}
-
-export function isMatch(input, answer) {
-  return normalizeText(input) === normalizeText(answer);
-}
