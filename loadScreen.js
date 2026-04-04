4894f88dfddd106f0a9f555ea9ebfb04894f76d0
arunansoundra-coder <arunansoundra@gmail.com>
2026-04-04 18:15:28 -0500

    Update loadScreen.js

--- loadScreen.js
+++ loadScreen.js
@@ -1,15 +1,24 @@
+import { startDashboard } from "./dashboard.js";
 import { startBriefing } from "./briefingUI.js";
 import { startMissionObjective } from "./missionObjective.js";
 import { startPoker } from "./pokerUI.js";
+import { startProposal } from "./proposal.js";
 
 export function loadScreen(screenName, state = {}) {
   const app = document.getElementById("app");
 
-  const next = (nextScreen) => {
-    loadScreen(nextScreen, state);
+  const next = (nextScreen, newState = {}) => {
+    loadScreen(nextScreen, { ...state, ...newState });
   };
 
   switch (screenName) {
+
+    case "dashboard":
+      startDashboard(app, {
+        loadScreen: (screen, s) => loadScreen(screen, s || state)
+      });
+      break;
+
     case "briefing":
       startBriefing(app, {
         next: () => next("mission"),
@@ -27,17 +36,19 @@ export function loadScreen(screenName, state = {}) {
     case "poker":
       startPoker(app, {
         state,
-        next: () => next("end")
+        next: () => next("proposal")
       });
       break;
 
-    case "end":
-      app.innerHTML = `
-        <div class="screen">
-          <h2>MISSION COMPLETE</h2>
-          <p>Operation concluded successfully.</p>
-        </div>
-      `;
+    case "proposal":
+      startProposal(app, {
+        state,
+        next: () => next("dashboard")
+      });
+      break;
+
+    case "questions":
+      app.innerHTML = "<h2>Questions Screen (deprecated - replace with module)</h2>";
       break;
 
     default:
