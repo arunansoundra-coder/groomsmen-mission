49bc34971a13e8cbfebd91cc73b7fb80b1945ffd
arunansoundra-coder <arunansoundra@gmail.com>
2026-04-04 18:29:57 -0500

    Delete roles.js

--- roles.js
+++
@@ -1,9 +0,0 @@
-export const roles = {
-  Jason: "Best Man",
-  Arunan: "Groom",
-  Gill: "Groomsman",
-  Prathap: "Groomsman",
-  Taylor: "Groomsman",
-  Duran: "Groomsman",
-  Josh: "Groomsman"
-};
