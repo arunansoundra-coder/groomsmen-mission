10a8c1c1107b821886a933c3e4d9e9afd82c4e3a
arunansoundra-coder <arunansoundra@gmail.com>
2026-04-19 14:27:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+import { renderAuth, renderBriefing } from "./ui.js";
 import { renderPoker } from "./poker.js";
 
 const app = document.getElementById("app");
 
-renderPoker(app);
+// change per user
+const agent = "Jason";
+
+renderAuth(app, () => {
+  renderBriefing(app, agent, () => {
+    renderPoker(app);
+  }, agent);
+});
